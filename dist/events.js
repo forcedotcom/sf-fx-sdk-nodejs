34e0dc9e08102e6e9325a494b2cc0e2f4addf071
Chris <cwall@salesforce.com>
2019-01-24 09:19:25 -0600

    Include dist/ since we aren't publishing to npm

--- dist/events.js
+++ dist/events.js
@@ -24,7 +24,10 @@ class EventManager {
     }
     initConsumer() {
         this.logger.log('Initializing Kafka consumer');
-        fs.mkdirSync('.certs');
+        const certDir = '.cert';
+        if (!fs.existsSync(certDir)) {
+            fs.mkdirSync(certDir);
+        }
         const trustedCert = path.join('.certs', 'KAFKA_TRUSTED_CERT');
         fs.writeFileSync(trustedCert, this.config.getBrokerTrustedCert());
         const clientCert = path.join('.certs', 'KAFKA_CLIENT_CERT');
@@ -40,10 +43,10 @@ class EventManager {
             'enable.auto.commit': false,
             'metadata.broker.list': this.brokers,
             'security.protocol': 'SSL',
-            // SSL certs written by `.profile` script.
+            // SSL certs written above to .cert/
             'ssl.ca.location': trustedCert,
             'ssl.certificate.location': clientCert,
-            'ssl.key.location': '.certs/KAFKA_CLIENT_CERT_KEY',
+            'ssl.key.location': clientCertKey,
         };
         if (this.config.isFinest()) {
             kafkaConfig['debug'] = 'all';
@@ -54,15 +57,30 @@ class EventManager {
         if (topicNames.length < 1) {
             throw new Error('Requires array of topicNames.');
         }
+        // TODO: query provider to get a list of available topics and cross-ref
+        // against given subscribe-to topics to ensure (a) topic's viability  and
+        // (b) correct casing (topics are case-sensitive)
         const prefix = this.config.getEventPrefix();
-        const kafkaTopics = topicNames.map(topic => `${prefix}${topic}`);
+        const kafkaTopics = topicNames.map(topic => 
+        // connect event topics are lowercase
+        `${prefix}${topic.toLowerCase()}`);
         this.logger.shout(`Consuming Kafka ${kafkaTopics.length === 1 ? 'topic' : 'topics'}: ${kafkaTopics.join(', ')}; group: ${groupId}`);
+        this.logger.log(`Ensure that group '${groupId}' is an active consumer group`);
         const connectTimeout = this.config.getBrokerTimeout();
         const connectTimoutFx = setTimeout(() => {
             const message = `Failed to connect Kafka consumer (${connectTimeout}-ms timeout)`;
             throw new Error(message);
         }, connectTimeout);
         this.consumer.connect();
+        this.consumer.on('event.log', log => {
+            this.logger.log(`📋 Kafka consumer event: ${JSON.stringify(log)}`);
+        });
+        this.consumer.on('event.error', err => {
+            this.logger.error(`!      Kafka consumer error: ${err.stack}`);
+        });
+        this.consumer.on('disconnected', (arg) => {
+            this.logger.error(`!      Kafka consumer disconnected: ${JSON.stringify(arg)}`);
+        });
         this.consumer.on('ready', (id, metadata) => {
             this.consumer.on('error', err => {
                 this.logger.log(`!      Error in Kafka consumer: ${err.stack}`);
@@ -80,7 +98,17 @@ class EventManager {
                 const event = value.data;
                 const context = await sdk.Context.create(event.payload, this.logger);
                 const name = value.channel.substring(value.channel.lastIndexOf('/') + 1);
-                await this.fx.invoke(new sdk.Event(name, context, event.payload));
+                // invoke function
+                try {
+                    await this.fx.invoke(new sdk.Event(name, context, event.payload));
+                }
+                catch (err) {
+                    // REVIEWME: to commit or not?
+                    this.logger.error(`!      Function error: ${err.message}`);
+                    this.logger.error(`!      ${err.stack}`);
+                }
+                // REVIEWME: function should determine if we commit
+                // the message or not
                 let didCommit = false;
                 let commitOnce = () => {
                     if (!didCommit) {
@@ -93,12 +121,6 @@ class EventManager {
             catch (err) {
                 this.logger.error(`!      Error consuming Kafka message: ${err.stack}`);
             }
-        });
-        this.consumer.on('event.log', log => {
-            this.logger.log(`📋 Kafka consumer event: ${JSON.stringify(log)}`);
-        });
-        this.consumer.on('event.error', err => {
-            this.logger.error(`!      Kafka consumer error: ${err.stack}`);
         });
     }
 }
