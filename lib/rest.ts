f10ac9cc7b7aafdf846890239a2f54be4a0ce9cf
Jeff <jbartolotta@salesforce.com>
2019-03-29 17:35:44 -0700

    Updated with 220 schema

--- lib/rest.ts
+++ lib/rest.ts
@@ -42,17 +42,33 @@ export default class RestManager {
             },
         );
 
+        /*  Example body sent from the dispatcher
+            {
+                "context": { 
+                    "userContext": { 
+                        "orgId": "00Dxx0000002967", 
+                        "userId": "005xx000001WZtP", 
+                        "username": "admin@9158457974503.com", 
+                        "salesforceBaseUrl": "http://jbartolott-wsl1.internal.salesforce.com:6109", 
+                        "orgDomainUrl": null, "sessionId": "00Dxx0000002967!AQcAQGp3QNDZkXyaD8n0CIhKWXnpudTACB_2S3Nq4Hn5aMBuz8RJGhGGSSDZn2X.96_DBs7hSY2K4cEP45a7jlJAP2C_yR7F" }, 
+                        "apiVersion": "46.0", "functionInvocationId": "9mdxx000000001Y" 
+                    }, 
+                    "payload": { "Account_ID__c": "001xx000003EHKn" } 
+                } 
+            }
+        */
         server.post(
             '/invoke',
             async (req: any, res: any, next: Function): Promise<any> => {
-                const payload = req.body;
+                const body = req.body;
 
                 try {
-                    const context = await sdk.Context.create(payload, logger);
+                    const context = await sdk.Context.create(body, logger);
                     const name = 'http';
-                    const result = await this.fx.invoke(new sdk.Event(name, context, payload));
+                    const result = await this.fx.invoke(new sdk.Event(name, context, body.payload));
                     res.send(200, result);
                 } catch (err) {
+                    logger.error(`Error: ${err}`);
                     res.send(500, err.message);
                 }
 
