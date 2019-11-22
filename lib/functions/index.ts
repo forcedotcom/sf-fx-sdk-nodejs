337a716583007fcec8951bd04736bb756f39e0f7
Jing Qian <jqian@salesforce.com>
2019-11-22 18:32:03 -0500

    0.0.1-develop.7.1

--- lib/functions/index.ts
+++ lib/functions/index.ts
@@ -4,24 +4,7 @@ import { ErrorResult, ForceApi, Logger, SObject, SuccessResult, UnitOfWork } fro
  * Represents invoking user.
  */
 export class UserContext {
-    public static create(context: any): UserContext {
-        const userContext = context.userContext;
-        if (!userContext) {
-            const message = `UserContext not provided: ${JSON.stringify(context)}`;
-            throw new Error(message);
-        }
-
-        return new UserContext(
-            userContext.orgDomainUrl,
-            userContext.orgId,
-            userContext.salesforceBaseUrl,
-            userContext.username,
-            userContext.userId,
-            userContext.onBehalfOfUserId
-        );
-    }
-
-    private constructor(
+    public constructor(
         public readonly orgDomainUrl: string,
         public readonly orgId: string,
         public readonly salesforceBaseUrl: string,
@@ -29,6 +12,22 @@ export class UserContext {
         public readonly userId: string,
         public readonly onBehalfOfUserId?: string,
     ) {}
+}
+
+/**
+ * Respresents the context of the function invocation including objects needed by functions to
+ * perform their action(s), eg ForceAPI to interact with the invoking org.
+ */
+export class Context {
+    public constructor(
+        public readonly apiVersion: string,
+        public readonly userContext: UserContext,
+        public readonly logger: Logger,
+        public readonly payloadVersion: string,
+        public readonly forceApi?: ForceApi,
+        public readonly unitOfWork?: UnitOfWork,
+        public readonly fxInvocation?: FunctionInvocationRequest
+        ) { }
 }
 
 // If an accessToken is provided, helper class for saving function response to FunctionInvocationRequest.Response.
@@ -76,20 +75,4 @@ export class FunctionInvocationRequest {
             throw new Error('Authorization not provided');
         }
     }
-}
-
-/**
- * Respresents the context of the function invocation including objects needed by functions to
- * perform their action(s), eg ForceAPI to interact with the invoking org.
- */
-export class Context {
-    public constructor(
-        public readonly apiVersion: string,
-        public readonly userContext: UserContext,
-        public readonly logger: Logger,
-        public readonly payloadVersion: string,
-        public readonly forceApi?: ForceApi,
-        public readonly unitOfWork?: UnitOfWork,
-        public readonly fxInvocation?: FunctionInvocationRequest
-        ) { }
 }
