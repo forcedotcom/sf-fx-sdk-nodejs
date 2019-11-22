85ad523188eb0945843b5c43102e13e6211b4735
Jing Qian <jqian@salesforce.com>
2019-11-22 15:26:30 -0500

    refactor code to middleware

--- lib/functions/index.ts
+++ lib/functions/index.ts
@@ -1,46 +1,4 @@
-import { ConnectionConfig, Constants, ErrorResult, ForceApi, Logger, SObject, SuccessResult, UnitOfWork } from '..';
-
-
-// TODO: Migrate to an Evergreen middleware layer.  Convenience method to not dupe
-// code across existing functions.
-// Once SF middleware is in place, the event param will be the function's
-// payload and context will be a fully setup sdk.Context instance.
-// Until then, event is a CloudEvent object that we'll parse either to
-// setup sdk.Context.
-export function applySfFxMiddleware(event: any): any {
-
-    if (!event) {
-        throw new Error('Data not provided');
-    }
-
-    const data = event.data;
-    if (!data) {
-        throw new Error('Data not provided');
-    }
-
-    // Again, this is temp: context param will be fully setup sdk.Context instance.
-    const context = data.context;
-    if (!context) {
-        throw new Error('Context not provided');
-    }
-
-    // Not all functions will require an accessToken used for org access.
-    // The accessToken will not be passed directly to functions, but instead
-    // passed as part of SFContext used in SF middleware to setup API instances.
-    let accessToken;
-    let functionInvocationId;
-    if (data.sfContext) {
-        accessToken = data.sfContext.accessToken || undefined;
-        functionInvocationId = data.sfContext.functionInvocationId || undefined;
-        // Internal only
-        delete data.sfContext;
-    }
-
-    // Transformed CloudEvent into function consumable payload (event) and context.
-    return {
-        context: Context.create(context, Logger.create(true), accessToken, functionInvocationId),
-        event: data.payload };
-}
+import { ErrorResult, ForceApi, Logger, SObject, SuccessResult, UnitOfWork } from '..';
 
 /**
  * Represents invoking user.
@@ -125,42 +83,7 @@ export class FunctionInvocationRequest {
  * perform their action(s), eg ForceAPI to interact with the invoking org.
  */
 export class Context {
-    // context contains salesforce stuff (user context, etc)
-    public static create(context: any, logger: Logger, accessToken?: string, functionInvocationId?: string): Context {
-        if (!context) {
-            throw new Error('Context not provided.');
-        }
-
-        if (typeof context === 'string') {
-            context = JSON.parse(context);
-        }
-
-        const userCtx = UserContext.create(context);
-        const apiVersion = context.apiVersion || process.env.FX_API_VERSION || Constants.CURRENT_API_VERSION;
-
-        // If accessToken was provided, setup APIs.
-        let forceApi: ForceApi;
-        let unitOfWork: UnitOfWork;
-        let fxInvocation: FunctionInvocationRequest;
-        if (accessToken) {
-            const config: ConnectionConfig = new ConnectionConfig(
-                accessToken,
-                apiVersion,
-                userCtx.salesforceBaseUrl
-            );
-            unitOfWork = new UnitOfWork(config, logger);
-            forceApi = new ForceApi(config, logger);
-
-            if (functionInvocationId) {
-                fxInvocation = new FunctionInvocationRequest(functionInvocationId, logger, forceApi);
-            }
-        }
-
-        return new Context(apiVersion, userCtx, logger, context.payloadVersion, forceApi, unitOfWork,
-            fxInvocation);
-    }
-
-    private constructor(
+    public constructor(
         public readonly apiVersion: string,
         public readonly userContext: UserContext,
         public readonly logger: Logger,
