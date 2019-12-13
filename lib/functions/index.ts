4c0f871525151877b76ada4f24fb731b4e6b94b9
Jing Qian <jqian@salesforce.com>
2019-12-13 15:28:33 -0500

    move FunctionInvocationRequest definition to middleware...

--- lib/functions/index.ts
+++ lib/functions/index.ts
@@ -1,4 +1,4 @@
-import { ErrorResult, ForceApi, Logger, SObject, SuccessResult, UnitOfWork } from '..';
+import { ForceApi, Logger, UnitOfWork } from '..';
 
 /**
  * Represents invoking user.
@@ -26,53 +26,6 @@ export class Context {
         public readonly payloadVersion: string,
         public readonly forceApi?: ForceApi,
         public readonly unitOfWork?: UnitOfWork,
-        public readonly fxInvocation?: FunctionInvocationRequest
+        public readonly fxInvocation?: any
         ) { }
-}
-
-// If an accessToken is provided, helper class for saving function response to FunctionInvocationRequest.Response.
-// TODO: Remove when FunctionInvocationRequest is deprecated.
-export class FunctionInvocationRequest {
-    public response: any;
-    public status: string;
-
-    constructor(public readonly id: string, private readonly logger: Logger, private readonly forceApi?: ForceApi) {
-    }
-
-    /**
-     * Saves FunctionInvocationRequest either through API w/ accessToken.
-     *
-     * @throws err if response not provided or on failed save
-     */
-    public async save(): Promise<any> {
-        if (!this.response) {
-            throw new Error('Response not provided');
-        }
-
-        if (this.forceApi) {
-            const responseBase64 = Buffer.from(JSON.stringify(this.response)).toString('base64');
-
-            try {
-                // Prime pump (W-6841389)
-                const soql = `SELECT Id, FunctionName, Status, CreatedById, CreatedDate FROM FunctionInvocationRequest WHERE Id ='${this.id}'`;
-                await this.forceApi.query(soql);
-            } catch (err) {
-                this.logger.warn(err.message);
-            }
-
-            const fxInvocation = new SObject('FunctionInvocationRequest').withId(this.id);
-            fxInvocation.setValue('ResponseBody', responseBase64);
-            const result: SuccessResult | ErrorResult = await this.forceApi.update(fxInvocation);
-            if (!result.success && 'errors' in result) {
-                // Tells tsc that 'errors' exist and join below is okay
-                const msg = `Failed to send response [${this.id}]: ${result.errors.join(',')}`;
-                this.logger.error(msg);
-                throw new Error(msg);
-            } else {
-                return result;
-            }
-        } else {
-            throw new Error('Authorization not provided');
-        }
-    }
 }
