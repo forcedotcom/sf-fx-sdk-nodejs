1ab32548b7519f24b661ad7da4af1768d92a276e
Lillian Zhang <zhang.l@salesforce.com>
2021-07-14 14:14:02 -0700

    add codeowners, remove scripts, add license header

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'header',
   ],
   extends: [
     'eslint:recommended',
@@ -14,5 +15,11 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     'semi': 'off',
     '@typescript-eslint/semi': ['error'],
+    "header/header": [2, "line", [
+      "Copyright (c) 2021, salesforce.com, inc.",
+      "All rights reserved.",
+      "SPDX-License-Identifier: BSD-3-Clause",
+      "For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause"
+    ]],
   }
 };
